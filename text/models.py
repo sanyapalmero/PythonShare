ae8f89d4b18367ff64b8d2f8f06bb2de08e63cb2
Alexandr Vagner <alexmednogorsk@mail.ru>
2018-09-19 20:36:53 +0500

    Register text model in admin panel and add property for showing short text

--- text/models.py
+++ text/models.py
@@ -1,4 +1,16 @@
 from django.db import models
+from django.contrib import admin
+from django.template.defaultfilters import truncatewords
+
 
 class Text(models.Model):
-    text = models.CharField(max_length = 100)
+    text = models.CharField(max_length=100)
+
+    @property
+    def short_text(self):
+        return truncatewords(self.text, 4)
+
+
+@admin.register(Text)
+class TextAdmin(admin.ModelAdmin):
+    list_display = ('id', 'short_text')
