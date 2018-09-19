56ef3a8a68069c3fd3b15a0216e70b6bca951537
Alexandr Vagner <alexmednogorsk@mail.ru>
2018-09-19 23:44:00 +0500

    Update urls for edit text models

--- text/urls.py
+++ text/urls.py
@@ -6,5 +6,7 @@ app_name = 'text'
 urlpatterns = [
     path('', views.index, name='index'),
     path('add/', views.add, name='add'),
+    path('upd/<int:text_id>', views.upd, name='upd'),
     path('text/<int:text_id>', views.detail, name='detail'),
+    path('edit/<int:text_id>', views.edit, name='edit'),
 ]
