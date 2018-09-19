901a562ea6671382208dd100d398f2a67e56560f
Alexandr Vagner <alexmednogorsk@mail.ru>
2018-09-19 23:38:50 +0500

    Add foreign key in model and add migration for creating user's field

--- text/models.py
+++ text/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib import admin
+from django.contrib.auth.models import User
 from django.template.defaultfilters import truncatewords
 
 
 class Text(models.Model):
     text = models.CharField(max_length=100)
+    user = models.ForeignKey(
+        User, null=True, blank=True, on_delete=models.CASCADE)
 
     @property
     def short_text(self):
