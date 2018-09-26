b95758eac9f7c9370f7c7cc3548cfb353f2ae6fd
Alexandr Vagner <alexmednogorsk@mail.ru>
2018-09-26 20:46:39 +0500

    Add using class-based views and update access control

--- text/urls.py
+++ text/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'text'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('add/', views.add, name='add'),
-    path('upd/<int:text_id>', views.upd, name='upd'),
-    path('text/<int:text_id>', views.detail, name='detail'),
-    path('edit/<int:text_id>', views.edit, name='edit'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('add/', views.AddView.as_view(), name='add'),
+    path('upd/<int:text_id>', views.UpdView.as_view(), name='upd'),
+    path('text/<int:text_id>', views.DetailView.as_view(), name='detail'),
+    path('edit/<int:text_id>', views.EditView.as_view(), name='edit'),
 ]
--- text/views.py
+++ text/views.py
@@ -1,38 +1,49 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseForbidden
+from django.views.generic import TemplateView
+from django.views import View
 from . import models
 
 
-def index(request):
-    texts = models.Text.objects.all()
-    return render(request, 'text/index.html', {'texts': texts})
+class IndexView(TemplateView):
+    template_name = 'text/index.html'
+
+    def get_context_data(self):
+        texts = models.Text.objects.all()
+        return {'texts': texts}
 
 
-def add(request):
-    text_post = request.POST['textfield']
-    text_obj = models.Text(text=text_post, user=request.user)
-    text_obj.save()
-    return redirect('text:detail', text_id=text_obj.id)
+class DetailView(TemplateView):
+    template_name = 'text/detail.html'
 
-
-def detail(request, text_id):
-    text = get_object_or_404(models.Text, id=text_id)
-    return render(request, 'text/detail.html', {'text': text})
-
-
-def upd(request, text_id):
-    text = get_object_or_404(models.Text, id=text_id)
-    try:
-        text.text = request.POST['textfield']
-        text.save()
-        return redirect('text:detail', text_id=text.id)
-    except KeyError:
-        return HttpResponseForbidden()
-
-
-def edit(request, text_id):
-    if request.user.is_authenticated:
+    def get_context_data(self, text_id):
         text = get_object_or_404(models.Text, id=text_id)
-        return render(request, 'text/edit.html', {'text': text})
-    else:
-        return HttpResponseForbidden()
+        return {'text': text}
+
+
+class EditView(View):
+    def get(self, request, text_id):
+        text = get_object_or_404(models.Text, id=text_id)
+        if request.user == text.user:
+            return render(request, 'text/edit.html', {'text': text})
+        else:
+            return HttpResponseForbidden()
+
+
+class AddView(View):
+    def post(self, request):
+        text_post = request.POST['textfield']
+        text_obj = models.Text(text=text_post, user=request.user)
+        text_obj.save()
+        return redirect('text:detail', text_id=text_obj.id)
+
+
+class UpdView(View):
+    def get(self, request, text_id):
+        text = get_object_or_404(models.Text, id=text_id)
+        if request.user == text.user:
+            text.text = request.POST['textfield']
+            text.save()
+            return redirect('text:detail', text_id=text.id)
+        else:
+            return HttpResponseForbidden()
