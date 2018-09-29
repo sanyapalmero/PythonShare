02d15094b53826739a6bf94113efc2ac5df1fbef
Alexandr Vagner <alexmednogorsk@mail.ru>
2018-09-30 00:34:49 +0500

    Add CreateView and return url for redirect after text deleting

--- text/views.py
+++ text/views.py
@@ -7,7 +7,7 @@ from django.core.paginator import Paginator
 from . import forms
 
 ENTRIES_COUNT = 20
-
+RETURN_URL = {}
 
 class IndexView(View):
     def get(self, request):
@@ -15,12 +15,19 @@ class IndexView(View):
         paginator = Paginator(texts_list, ENTRIES_COUNT)
         page = request.GET.get('page')
         texts = paginator.get_page(page)
-        form = forms.TextForm()
+        if page == None:
+            return_url = '?page=1'
+        else:
+            return_url = f'?page={page}'
+        RETURN_URL["url"] = return_url
         return render(request, 'text/index.html', {
             'texts': texts,
-            'form': form
         })
 
+class CreateView(View):
+    def get(self, request):
+        form = forms.TextForm()
+        return render(request, 'text/create.html', {'form': form})
 
 class DetailView(TemplateView):
     template_name = 'text/detail.html'
@@ -56,7 +63,8 @@ class DelView(View):
         text = get_object_or_404(models.Text, id=text_id)
         if request.user == text.user:
             text.delete()
-            return redirect('text:index')
+            return_url = RETURN_URL["url"]
+            return redirect (f'/{return_url}')
         else:
             return HttpResponseForbidden()
 
