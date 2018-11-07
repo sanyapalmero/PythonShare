669aab8a31b90e309242d036ed8808973e904fe4
Alexandr Vagner <alexmednogorsk@mail.ru>
2018-11-07 22:04:32 +0500

    Add local_settings and remove path to config file from DATABASES

--- main/settings.py
+++ main/settings.py
@@ -77,10 +77,7 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS':{
-		'read_default_file':'/home/palmero/PythonShare/my.cnf',
-	}
+        'ENGINE': 'django.db.backends.mysql'
     }
 }
 
@@ -135,3 +132,8 @@ LOGIN_URL = '/user/login/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
